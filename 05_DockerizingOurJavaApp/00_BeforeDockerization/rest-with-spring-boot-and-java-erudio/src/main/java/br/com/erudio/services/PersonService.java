a0651afbce3380b0ee34e862fd4faf60087c8406
leandrocgsi <leandrocgsi@gmail.com>
2025-10-28 23:13:14 -0300

    Before - Dockerizing our RESTfull API Application

--- 05_DockerizingOurJavaApp/00_BeforeDockerization/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/services/PersonService.java
+++ 05_DockerizingOurJavaApp/00_BeforeDockerization/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/services/PersonService.java
@@ -161,9 +161,6 @@ public class PersonService {
         entity.setLastName(person.getLastName());
         entity.setAddress(person.getAddress());
         entity.setGender(person.getGender());
-        entity.setEnabled(person.getEnabled());
-        entity.setPhotoUrl(person.getPhotoUrl());
-        entity.setProfileUrl(person.getProfileUrl());
 
         var dto = parseObject(repository.save(entity), PersonDTO.class);
         addHateoasLinks(dto);
